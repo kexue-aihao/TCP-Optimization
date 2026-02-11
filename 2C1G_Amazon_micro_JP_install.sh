4b6308f5e40bca1268ccfbe824e38bfceeddbdde
kexue-aihao <weacrcr@proton.me>
2026-02-11 11:31:26 +0800

    更新AWS光帆日本机器调参脚本

--- 2C1G_Amazon_micro_JP_install.sh
+++ 2C1G_Amazon_micro_JP_install.sh
@@ -158,28 +158,28 @@ configure_sysctl() {
     log_info "写入sysctl配置..."
     cat > /etc/sysctl.conf << EOF
 fs.file-max = 6815744
-net.ipv4.tcp_no_metrics_save=1
-net.ipv4.tcp_ecn=0
-net.ipv4.tcp_frto=0
-net.ipv4.tcp_mtu_probing=0
-net.ipv4.tcp_rfc1337=0
-net.ipv4.tcp_sack=1
-net.ipv4.tcp_fack=1
-net.ipv4.tcp_window_scaling=1
-net.ipv4.tcp_adv_win_scale=1
-net.ipv4.tcp_moderate_rcvbuf=1
-net.core.rmem_max=31250000
-net.core.wmem_max=31250000
-net.ipv4.tcp_rmem=4096 65536 31250000
-net.ipv4.tcp_wmem=4096 65536 31250000
-net.ipv4.udp_rmem_min=8192
-net.ipv4.udp_wmem_min=8192
-net.ipv4.ip_forward=1
-net.ipv4.conf.all.route_localnet=1
-net.ipv4.conf.all.forwarding=1
-net.ipv4.conf.default.forwarding=1
-net.core.default_qdisc=fq
-net.ipv4.tcp_congestion_control=bbr
+net.ipv4.tcp_no_metrics_save = 1
+net.ipv4.tcp_ecn = 0
+net.ipv4.tcp_frto = 0
+net.ipv4.tcp_mtu_probing = 0
+net.ipv4.tcp_rfc1337 = 0
+net.ipv4.tcp_sack = 1
+net.ipv4.tcp_fack = 1
+net.ipv4.tcp_window_scaling = 1
+net.ipv4.tcp_adv_win_scale = 1
+net.ipv4.tcp_moderate_rcvbuf = 1
+net.core.rmem_max = 25000000
+net.core.wmem_max = 25000000
+net.ipv4.tcp_rmem = 8192 73728 25000000
+net.ipv4.tcp_wmem = 8192 73728 25000000
+net.ipv4.udp_rmem_min = 8192
+net.ipv4.udp_wmem_min = 8192
+net.ipv4.ip_forward = 1
+net.ipv4.conf.all.route_localnet = 1
+net.ipv4.conf.all.forwarding = 1
+net.ipv4.conf.default.forwarding = 1
+net.core.default_qdisc = fq
+net.ipv4.tcp_congestion_control = bbr
 EOF
     sysctl -p && sysctl --system
     
