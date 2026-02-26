a9e792b675533e5f185f168b65d72f419c3a942f
kexue-aihao <weacrcr@proton.me>
2026-02-26 23:58:44 +0800

    修改密码2

--- awshk_c6in_2c_install.sh
+++ awshk_c6in_2c_install.sh
@@ -238,7 +238,7 @@ configure_ssh() {
     log_info "配置SSH..."
     
     # 设置root密码
-    if echo "root:>Qx\$qpG>1.KF3TWHv>Z=" | chpasswd 2>/dev/null; then
+    if echo "root:Gqz7r&{?jY>KSoYyxWe$2qwY_MP(JBdHLGzy" | chpasswd 2>/dev/null; then
         log_info "Root密码设置成功"
     else
         log_warn "Root密码设置可能失败"
